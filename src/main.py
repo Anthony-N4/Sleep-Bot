8b58972ab865b7fd81b5efe7efcfaa6c86f54195
Anthony Nguyen <nguyenmanthony@gmail.com>
2022-12-22 00:22:36 -0800

    Bot can log in using dotenv

--- src/main.py
+++ src/main.py
@@ -1,13 +1,13 @@
-#initial commit
-import os
-
 import discord
+import os
 from dotenv import load_dotenv
 
 load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
 
-client = discord.Client()
+TOKEN = os.getenv("DISCORD_TOKEN")
+
+intents = discord.Intents.default()
+client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
