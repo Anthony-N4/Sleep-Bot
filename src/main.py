3fde973777814bdf7f01e140c22ea3ea60e21aeb
Anthony Nguyen <nguyenmanthony@gmail.com>
2023-01-11 23:02:02 -0800

    using env now

--- src/main.py
+++ src/main.py
@@ -8,17 +8,18 @@ from discord.ext import commands
 load_dotenv()
 
 TOKEN = os.getenv("DISCORD_TOKEN")
+GUILD = os.getenv("GUILD_ID")
 bot = commands.Bot(activity = discord.Activity(type = discord.ActivityType.watching, name = "ou not sleep"), status = discord.Status.online)
 
-@bot.slash_command(name = "log-hours", description = "Logs when you start and stop sleeping", guild_ids = [965992554246049833])
+@bot.slash_command(name = "log-hours", description = "Logs when you start and stop sleeping", guild_ids = [GUILD])
 async def log_data(ctx, start: Option(str, description = "When did you start sleeping?", require = True), end: Option(str, description = "When did you wake up?")):
     await ctx.respond(f"Hours recorded. You slept from {start} to {end}. Nice!")
 
-@bot.slash_command(name = "weekly-average", description = "Returns the average amount of sleep you got this week", guild_ids = [965992554246049833])
+@bot.slash_command(name = "weekly-average", description = "Returns the average amount of sleep you got this week", guild_ids = [GUILD])
 async def log_data(ctx):
     await ctx.respond("Test")
 
-@bot.slash_command(name = "graph", description = "Returns a graph of how much sleep you got this month", guild_ids = [965992554246049833])
+@bot.slash_command(name = "graph", description = "Returns a graph of how much sleep you got this month", guild_ids = [GUILD])
 async def log_data(ctx):
     await ctx.respond("Test")
 
